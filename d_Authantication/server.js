dfdaba239b3f7b89301f2c7b37dac44fbd4e8d59
vaibhav7566 <vaibhavdhakad74@gmail.com>
2025-12-03 15:02:24 +0530

    kjj

--- d_Authantication/server.js
+++ d_Authantication/server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const connectDB = require("./config/db");
 const userRoute = require("./routes/user.route");
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
 
 const server = express();
 
 connectDB();
 server.use(express.json());
 
-server.use("/user",userRoute);
+server.use(cookieParser());
+
+server.use("/user", userRoute);
 
 server.listen(3000, () => {
   console.log("Server is running at port 3000");
